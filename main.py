eecc2d0ad5899193db8653978a081f192b05a2ff
Syahid Husein <92115178+syahidhusein@users.noreply.github.com>
2023-02-07 21:22:19 +0100

    created main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+## How to Play
+
+print("How to Play?")
+time.sleep(0.5)
+print("- The game is played on a 10x10 grid")
+time.sleep(0.5)
+print("- You will place your ships on the grid")
+time.sleep(0.5)
+print("- The opponent will place its ships on the grid")
+time.sleep(0.5)
+print("- Take turns trying to sink each other's ships")
+time.sleep(0.5)
+print("- The first player to sink all of their opponent's ships wins")
+
+"""
+Built With:
+
+- [Python](https://www.python.org/)
+
+Authors
+
+- Syahid Husein
+
+Acknowledgments
+
+- Inspiration from the classic board game
+"""
+
+print("Enjoy the game!")
+
+## Code starts here
